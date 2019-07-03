fcd4b72f618462a397ed649ca341e8b155d32f97
seun jay <seunjay92@gmail.com>
2019-07-04 00:36:32 +0100

    Import Body component in Home component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../components/layouts/Header"
+import Body from "../components/layouts/Body";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Body/>
     </div>
   )
 }

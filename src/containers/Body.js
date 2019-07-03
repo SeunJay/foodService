5079cb7952d6bc7e1fe22b7a9ad3ba375d4a5f9d
seun jay <seunjay92@gmail.com>
2019-07-04 00:31:07 +0100

    Create Body component

---
+++ src/containers/Body.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div>
+      <section
+        className="section-features js--section-features"
+        id="features"
+      >
+        <div className="row-flex">
+          <h2>Get food fast &mdash; not fast food</h2>
+          <p className="long-copy">
+            Hello, we're Omnifood, your new premium food delivery service. We
+            know you're always busy. No time for cooking. So let us take care
+            of that, we're really good at it, we promise!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}

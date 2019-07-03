8f3b80f9a96cc6ae694fb2b69b33b40be1af5f15
seun jay <seunjay92@gmail.com>
2019-07-03 14:50:31 +0100

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import Home from "./components/Home";
-import "./App.css"
-
-
+import Signup from "./components/layouts/Signup"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Signup/>
     </div>
   );
 }

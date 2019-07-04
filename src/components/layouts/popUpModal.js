853733493185469ab94d6514f386e41f29d2454a
seun jay <seunjay92@gmail.com>
2019-07-04 13:12:56 +0100

    Create popUpModal component

---
+++ src/components/layouts/popUpModal.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Login from "./Login";
+
+
+
+function getModalStyle() {
+  const top = 50 
+  const left = 50 
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4),
+    outline: "none",
+    background: "#fff"
+  }
+}));
+
+export default function PopUpModal(props) {
+  const [open, setOpen] = React.useState(false);
+  const { modalLoginOpen, setHeaderModal } = props;
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  useEffect(() => {
+    if (modalLoginOpen) {
+      setOpen(true);
+    }
+    return () => {};
+  }, [modalLoginOpen]);
+
+
+  const handleClose = () => {
+    setOpen(false);
+    setHeaderModal();
+  };
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <Login />
+        </div>
+      </Modal>
+    </div>
+  );
+}
